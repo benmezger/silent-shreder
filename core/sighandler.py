e27ebf861b20b23f290b5be97d8135f64e439180
Ben Mezger <me@benmezger.nl>
2017-03-30 11:12:30 -0300

    Check for those signals we can actually catch. Atexit now registers a cleanup function

--- core/sighandler.py
+++ core/sighandler.py
@@ -1,49 +1,33 @@
 import signal
 import atexit
+import sys
 
-sig_types = {
-        signal.SIGABRT: 'SIGABRT',
-        signal.SIGALRM: 'SIGALRM',
-        signal.SIGBUS: 'SIGBUS',
-        signal.SIGCHLD: 'SIGCHLD',
-        signal.SIGCONT: 'SIGCONT',
-        signal.SIGFPE: 'SIGFPE',
-        signal.SIGHUP: 'SIGHUP',
-        signal.SIGILL: 'SIGILL',
-        signal.SIGINT: 'SIGINT',
-        signal.SIGPIPE: 'SIGPIPE',
-        signal.SIGPOLL: 'SIGPOLL',
-        signal.SIGPROF: 'SIGPROF',
-        signal.SIGQUIT: 'SIGQUIT',
-        signal.SIGSEGV: 'SIGSEGV',
-        signal.SIGSYS: 'SIGSYS',
-        signal.SIGTERM: 'SIGTERM',
-        signal.SIGTRAP: 'SIGTRAP',
-        signal.SIGTSTP: 'SIGTSTP',
-        signal.SIGTTIN: 'SIGTTIN',
-        signal.SIGTTOU: 'SIGTTOU',
-        signal.SIGURG: 'SIGURG',
-        signal.SIGUSR1: 'SIGUSR1',
-        signal.SIGUSR2: 'SIGUSR2',
-        signal.SIGVTALRM: 'SIGVTALRM',
-        signal.SIGXCPU: 'SIGXCPU',
-        signal.SIGXFSZ: 'SIGXFSZ',
-        }
+kill_sigs = {
+    signal.SIGHUP: "SIGHUP",
+    signal.SIGINT: "SIGINT",
+    signal.SIGTERM: "SIGTERM",
+    signal.SIGQUIT: "SIGQUIT",
+    # TODO some signals are missing
+    }
 
 class SigHandler(object):
-    def __init__(self, cls, funcn):
-        # self.signals = [s for s in dir(signal) if s.startswith("SIG")]
+    def __init__(self, cls, funcn, debug=False):
         self.cls = cls
         self.funcn = funcn
+        self.debug = debug
 
     def setup(self):
         sgn = []
-        for num in sig_types:
+        for num in kill_sigs:
             sgn.append(signal.signal(num, self.handler))
             signal.siginterrupt(num, False)
 
-    @atexit.register
+        atexit.register(self.handler, None, None)
+
     def handler(self, signum, frame):
+        if self.debug:
+            sys.stdout.write("Running handler..\n")
+            return
         getattr(self.cls, self.funcn)()
 
 if __name__ == "__main__":
